a2d0a58b7e82ede7e9e8dffe8298f17f331d758d
jorge.paredes <j_estebanps@hotmail.com>
2025-06-24 16:00:40 -0300

    Estructura inicial de la aplicacion

---
+++ src/main/java/com/stchallenge/StchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.stchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stchallenge/StchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
