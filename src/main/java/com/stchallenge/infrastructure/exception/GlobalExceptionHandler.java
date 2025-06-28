03595699ac739b867080a3b1f642f0ca27612864
jorge.paredes <j_estebanps@hotmail.com>
2025-06-28 15:11:06 -0300

    Mejoras en manejador global de validaciones.

--- src/main/java/com/stchallenge/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stchallenge/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,17 +1,15 @@
 package com.stchallenge.infrastructure.exception;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,12 +17,15 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-		Map<String, List<String>> body = new HashMap<>();
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream()
-				.map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+	       String mensaje = ex.getBindingResult().getFieldErrors().stream()
+	               .map(DefaultMessageSourceResolvable::getDefaultMessage)
+					.collect(Collectors.joining(", "));
 
-		body.put("Request validation errors", errors);
+			return ResponseEntity.badRequest()
+					.body(errorBody(HttpStatus.BAD_REQUEST.value(), "Request validation error.", mensaje));
+		}
 
-		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-	}
+		private Map<String, Object> errorBody(int status, String error, String message) {
+			return Map.of("status", status, "error", error, "message", message);
+		}
 }
